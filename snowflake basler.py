b307525076aeaadfd4dfaab4eaf919d66d870ea5
AFBasler <91565782+AFBasler@users.noreply.github.com>
2021-10-04 15:10:32 -0500

    Add files via upload

--- snowflake basler.py
+++ snowflake basler.py
@@ -4,10 +4,11 @@ abby = turtle.Turtle()
 
 #Functions
 def randomDot():
-    abby.circle(50)
-    abby.color("blue")
     abby.begin_fill()
+    abby.color("lavender")
+    abby.circle(100)
     abby.end_fill()
 
 #main
-    randomDot()
+randomDot()
+
